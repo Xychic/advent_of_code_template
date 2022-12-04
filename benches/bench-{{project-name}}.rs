5d840bad22ec78ab01cd3c9f13a96d39f6f24536
Jacob Turner <jacob11turner@gmail.com>
2022-12-04 23:11:57 +0000

    Create template

---
+++ benches/bench-{{project-name}}.rs
@@ -0,0 +1,13 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use {{project-name}}::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let raw_input = fs::read_to_string("../../input.txt").expect("error reading file");
+    let input = parse(&raw_input);
+    c.bench_function("{{year}}::Day{{day}}::parse", |b| b.iter(|| parse(&raw_input)));
+    c.bench_function("{{year}}::Day{{day}}::part 1", |b| b.iter(|| part_1(&input)));
+    c.bench_function("{{year}}::Day{{day}}::part 2", |b| b.iter(|| part_2(&input)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+type Input<'a> = Vec<&'a str>;
+
+pub fn parse(input: &str) -> Input {
+    dbg!(input.trim());
+    todo!()
+}
+
+pub fn part_1(input: &Input) -> usize {
+    dbg!(input);
+    todo!()
+}
+
+pub fn part_2(input: &Input) -> usize {
+    todo!()
+}
